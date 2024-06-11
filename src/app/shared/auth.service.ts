6f5acb2b169419bb3a5be791175e09d4bbbcdf96
Aavesh Momin <70098026+aavesh22@users.noreply.github.com>
2024-06-11 10:55:45 +0530

    Update auth.service.ts

--- src/app/shared/auth.service.ts
+++ src/app/shared/auth.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
-// import { GoogleAuthProvider, GithubAuthProvider, FacebookAuthProvider } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 
 export class AuthService {
-  // private isAuthenticated: boolean = false;
 
   constructor(private router: Router, private fireauth: AngularFireAuth) { }
 
@@ -20,6 +18,15 @@ export class AuthService {
       return Promise.reject(err);
     });
   }
+
+  register(email: string, password: string) {
+    return this.fireauth.createUserWithEmailAndPassword(email, password).then(() => {
+      return Promise.resolve();
+    }, err => {
+      return Promise.reject(err);
+    });
+  }
+  
   logout() {
     this.fireauth.signOut().then(() => {
       localStorage.removeItem('token');
@@ -29,46 +36,4 @@ export class AuthService {
       alert(err.message);
     });
   }
-
-  // isAuthenticatedUser(): boolean {
-  //     return this.isAuthenticated;
-  // }
-
-  // register method
-  register(email: string, password: string) {
-    return this.fireauth.createUserWithEmailAndPassword(email, password).then(() => {
-      return Promise.resolve();
-    }, err => {
-      return Promise.reject(err);
-    });
-  }
-  //   // forgot password
-  //   forgotPassword(email: string) {
-  //     this.fireauth.sendPasswordResetEmail(email).then(() => {
-  //       this.router.navigate(['/verify-email']);
-  //     }, err => {
-  //       alert('Something went wrong');
-  //     })
-  //   }
-
-  //   // email Verification
-  //   sendEmailForVerification(user: any) {
-  //     console.log(user);
-  //     user.sendEmailVerification().then((res: any) => {
-  //       this.router.navigate(['/verify-email']);
-  //     }, (err: any) => {
-  //       alert('Something went wrong. Not able to send mail to your email.')
-  //     })
-  //   }
-
-  //   //SigninwithGoogle
-  //   googleSignIn() {
-  //     return this.fireauth.signInWithPopup(new GoogleAuthProvider).then((res) => {
-  //       this.router.navigate(['/dashboard']);
-  //       localStorage.setItem('token', JSON.stringify(res.user?.uid));
-  //     }, err => {
-  //       alert(err.message);
-  //     }
-  //     )
-  //   }
 }
